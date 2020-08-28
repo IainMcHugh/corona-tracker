af56d9e89c1c01f75de0cb723e3a5389c2aac6ab
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2020-08-28 10:41:24 +0100

    Updates

---
+++ precache-manifest.47f6a3a7c57240aa64620865f0b62064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d17fd00e69774966aa8a3938261cc2f",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "a2b20d1994dddb8d036e",
+    "url": "/corona-tracker/static/css/main.941483e0.chunk.css"
+  },
+  {
+    "revision": "627f6a87c44bf071a68d",
+    "url": "/corona-tracker/static/js/2.f633efe9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.f633efe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b20d1994dddb8d036e",
+    "url": "/corona-tracker/static/js/main.26693943.chunk.js"
+  },
+  {
+    "revision": "3978a030d47e50bd1e16",
+    "url": "/corona-tracker/static/js/runtime-main.9a79d275.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
